ec6d9247f9cfd5b8069f6d42790a94c797578240
noahhodge04 <noahhodge04@gmail.com>
2026-01-30 13:19:18 -0500

    Added matches and updated routes

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,16 +1,13 @@
 class MatchesController < ApplicationController
-  # GET /socks/new
+  # GET /socks/{:sock_id}/matches/new
   def new
-    @sock = Sock.new
-  end
-
-  # GET /socks/1/edit
-  def edit
+    @match = Match.new
+    @socks = Sock.all
   end
 
   # POST /socks or /socks.json
   def create
-    @sock = Sock.new(sock_params)
+    @sock = Sock.find(sock_params[:sock_id])
 
     respond_to do |format|
       if @sock.save
@@ -23,38 +20,9 @@ class MatchesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /socks/1 or /socks/1.json
-  def update
-    respond_to do |format|
-      if @sock.update(sock_params)
-        format.html { redirect_to @sock, notice: "Sock was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @sock }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @sock.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /socks/1 or /socks/1.json
-  def destroy
-    @sock.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to socks_path, notice: "Sock was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_sock
-      @sock = Sock.find(params.expect(:id))
-    end
-
-    # Only allow a list of trusted parameters through.
-    def sock_params
-      params.expect(sock: [ :name, :description, :gender, :age, :height, :color ])
+    def load_sock
+      @sock = Sock.find(sock_params[:sock_id])
     end
 
 end
