4697cf3cf4a99d95641ca7ccf38a031f1f9cfab0
Nathan Feaver <nathan.feaver@gmail.com>
2026-02-03 10:22:04 -0500

    add socks and matches to seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,90 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+[
+  {
+    id: 1,
+    age: 4,
+    color: "green",
+    description: "I'm a quiet sock that enjoys long walks in the park.",
+    gender: "male",
+    height: "knee-high",
+    name: "Jerry"
+  },
+  {
+    id: 2,
+    age: 2,
+    color: "blue",
+    description: "A cheerful sock who hums to itself while tumbling in the dryer.",
+    gender: "female",
+    height: "ankle",
+    name: "Bubbles"
+  },
+  {
+    id: 3,
+    age: 7,
+    color: "red",
+    description: "Bold, dramatic, and absolutely convinced laundry day is a conspiracy.",
+    gender: "male",
+    height: "crew",
+    name: "Rocco"
+  },
+  {
+    id: 4,
+    age: 5,
+    color: "yellow",
+    description: "Loves sunshine, mismatched outfits, and spontaneous adventures.",
+    gender: "female",
+    height: "knee-high",
+    name: "Sunny"
+  },
+  {
+    id: 5,
+    age: 10,
+    color: "black",
+    description: "Minimalist sock with a deep appreciation for symmetry and order.",
+    gender: "non-binary",
+    height: "dress",
+    name: "Mono"
+  },
+  {
+    id: 6,
+    age: 1,
+    color: "pink",
+    description: "Still figuring things out but very optimistic about drawers.",
+    gender: "female",
+    height: "ankle",
+    name: "Peaches"
+  },
+  {
+    id: 7,
+    age: 8,
+    color: "striped",
+    description: "World traveler who claims to have seen every washing machine brand.",
+    gender: "male",
+    height: "crew",
+    name: "Marco"
+  },
+  {
+    id: 8,
+    age: 6,
+    color: "purple",
+    description: "Creative sock who dreams of becoming a scarf one day.",
+    gender: "non-binary",
+    height: "knee-high",
+    name: "Velour"
+  }
+].each do |sock_params|
+  id = sock_params.delete(:id)
+  sock = Sock.new
+  if id
+    sock = Sock.find_or_initialize_by(id: id)
+  end
+  sock.assign_attributes(sock_params)
+  sock.save
+end
+
+[[1, 4], [3, 7]].each do |match_id_1, match_id_2|
+  Match.find_or_create_by(sock_1_id: match_id_1, sock_2_id: match_id_2)
+end
